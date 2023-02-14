7cafff824835995830833467e132f088705b73ff
hokenz <dima.2016.carawanov@gmail.com>
2023-02-14 13:35:05 +0300

    more beautiful code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 fn to_base2(mut num: u32) -> u32 {
     let mut res: String = "".to_owned();
     while num != 0 {
-        let lol = (num%2).to_string();
-        res.push_str(&lol[..]);
+        res = format!("{0}{1}", res, num%2);
         num /= 2;
     }
     res = res.chars()
@@ -16,13 +15,17 @@ fn from_minus2(num: u32) -> i32 {
     let str_primary = num.to_string();
     let mut str: &str = &str_primary[..];
     let mut i = 0u32;
-    let rev_str = str
-                     .chars()
+    let rev_str = str.chars()
                      .rev()
                      .collect::<String>();
     str = &rev_str;
-    while i != str.chars().count().try_into().unwrap() {
-        let g = str.bytes().nth(i.try_into().unwrap()).unwrap();
+    while i != str.chars()
+                  .count()
+                  .try_into()
+                  .unwrap() {
+        let g = str.bytes().nth(i.try_into()
+                                            .unwrap())
+                                            .unwrap();
         if g == 49 {
             res += i32::pow(-2, i);
         }
@@ -43,14 +46,10 @@ fn to_minus2(num: i32) -> u32 {
             value += 1;
         }
         if rem == 1 {
-            let mut lol = "1".to_owned();
-            lol.push_str(&res);
-            res = lol;
+            res = format!("1{}", res);
         }
         if rem == 0 {
-            let mut lol = "0".to_owned();
-            lol.push_str(&res);
-            res = lol;
+            res = format!("0{}", res);
         }
     }
 
